4679a56a72b7ea912ae4021c56c11bdff1b9def3
Yashodha-Nesaragi <78290990+Yashodha-Nesaragi@users.noreply.github.com>
2021-02-11 13:05:44 +0530

    Updated SetInterfaceDemo with TreeSet

--- SetInterfaceDemo.java
+++ SetInterfaceDemo.java
@@ -1,6 +1,7 @@
 package day13;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.TreeSet;
 
 public class SetInterfaceDemo {
 
@@ -20,7 +21,29 @@ public class SetInterfaceDemo {
 		lh.add("java");
 		lh.add("Hello");
 		System.out.println("LinkedhashSet "+lh);
+		TreeSet t=new TreeSet();
+		t.add(10);
+		t.add(16);
+		t.add(5);
+		t.add(14);
+		System.out.println("TreeSet "+t);
+
+		TreeSet t1=new TreeSet();
+		t1.add(new String("Hello"));
+		t1.add("Hi");
+		t1.add("Apple");
+		System.out.println("TreeSet1 "+t1);
+
+
+		TreeSet<StringBuffer> t2=new TreeSet<StringBuffer>();
+		t2.add(new StringBuffer("Hello"));
+		t2.add(new StringBuffer("Apple"));
 		
+
+		System.out.println("TreeSet2 "+t2);
+
+
+
 
 	}
 
